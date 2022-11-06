8eb0ce62c6e52dc465f6f5344202cb878fab5e09
nixondeflin <89503892+nixondeflin@users.noreply.github.com>
2022-11-06 17:06:21 +0700

    Add files via upload

---
+++ listgame/listgame.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "listgame.h"
+#include "arraydin.h"
+#include "wordmachine.h"
+#include "boolean.h"
+
+void ListGame(ArrayDin *array){
+    printf("Berikut adalah daftar game yang tersedia\n");
+    int i;
+    for(i = 0 ; i <= (Length(*array)-1) ; i++ ){
+        printf("%d. %s\n",(i+1),array->A[i]);
+    }
+
+}
+
+//int i;
+//    for(i = 0 ; i <= (Length(*array)-1) ; i++ ){
+//        printf("%d. %s\n",(i+1),((*array).((*A)[i])));
+//    }
