88b37843d911d1267c90c90fdca9d3b6a86b3888
adtyainash <16521107@mahasiswa.itb.ac.id>
2022-11-08 16:50:13 +0700

    updated

---
+++ queuegame/quit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "quit.h"
+#include "queue.h"
+
+void quit(Queue *q){
+    printf("Anda keluar dari game BNMO");
+    printf("Bye bye ...");
+    CreateQueue(q);
+}
---
+++ queuegame/quit.h
@@ -0,0 +1,9 @@
+#include "boolean.h"
+
+
+#ifndef QUIT_H
+#define QUIT_H
+
+void quit();
+
+#endif
