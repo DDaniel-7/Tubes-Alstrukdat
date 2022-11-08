631118363bb27754b5743e51ff852c7162de6b7a
nixondeflin <89503892+nixondeflin@users.noreply.github.com>
2022-11-08 15:19:37 +0700

    update queuegame
    jangan lupa ./adt/ di #include

--- queuegame/queuegame.c
+++ queuegame/queuegame.c
@@ -3,20 +3,26 @@
 #include "queue.h"
 #include "arraydin.h"
 #include "wordmachine.h"
+#include "listgame.h"
 #include <stdio.h>
+#include "stdlib.h"
+
+
 void QueueGame(ArrayDin *array, Queue q){
-	// Createqueue(&q);
-	int i;
+	int i, nomorint; 
+	char* nomorgame;
     Word num;
-	for(i=0;i<(Length(*array));i++){
-		printf("%d. %s\n", (i+1), (&(*array).A[i]));
-	}
+	ListGame(array);
 	printf("Nomor game yang mau ditambahkan ke antrian : ");
-	// ScanNum(num);
-	if (ScanNum(num) >= 1 && ScanNum(num) <= (Length(*array)-1)){
-		enqueue(&q, (*array).A[ScanNum(num)]);
+	nomorgame = scaninput();
+	nomorint = atoi(nomorgame);
+	if (nomorint >= 1 && nomorint <= (Length(*array))){
+		enqueue(&q, (*array).A[nomorint-1]);
 		printf("Game berhasil ditambahkan kedalam daftar antrian.\n");
 	}
 	else {
 		printf("Nomor permainan tidak valid, silahkan masukkan nomor game pada list\n");
     }
+
+	displayQueue(q);
+}
