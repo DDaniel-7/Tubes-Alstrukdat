8032e1e8beec38bdff2c3f3b7d920d31dabb6278
nixondeflin <89503892+nixondeflin@users.noreply.github.com>
2022-11-09 23:42:29 +0700

    wda

--- assembled/main.c
+++ assembled/main.c
@@ -17,7 +17,7 @@ int main(){
 
     //VARIABEL
     char* input;
-    boolean exit = false;
+    boolean repeat = true;
 
     //FILE
     //char* namaFile = "listgame.txt";
@@ -43,97 +43,109 @@ int main(){
     printf("ENTER COMMAND : ");
     input = scaninput();
 
-    if(IsStrEq(startinput,input)){
-        start(&arraygame);
-        while(!IsStrEq(quitinput,input)){
-            printf("ENTER COMMAND : ");
-            input = scaninput();
+    while(repeat){
+        if(IsStrEq(startinput,input)){
+            repeat = false;
+            start(&arraygame);
+            while(!IsStrEq(quitinput,input)){
+                printf("ENTER COMMAND : ");
+                input = scaninput();
 
-            if(IsStrEq(saveinput,firstword(input))){
-                char* namaFile;
-                namaFile = secondword(input);
-                save(namaFile,arraygame);
-                //save(namaFile,&arraygame);
-            }
-            else if(IsStrEq(creategameinput,input)){
-                CreateGame(&arraygame);
-            }
-            else if(IsStrEq(listgameinput,input)){
-                ListGame(&arraygame);
-            }
-            else if(IsStrEq(deletegameinput,input)){
-                DeleteGame(&arraygame,&queue);
-            }
-            else if(IsStrEq(queuegameinput,input)){
-                QueueGame(&arraygame,&queue);
-            }
-            else if(IsStrEq(playgameinput,input)){
-                PlayGame(&queue);
-            }
-            else if(IsStrEq(helpinput,input)){
-                help();
-            }
-            else if(IsStrEq(skipgameinput,firstword(input))){
-                int manyskip;
-                manyskip = atoi(secondword(input));
-                SkipGame(&queue,manyskip);
-            }
-            else if(IsStrEq(quitinput,input)){
-                printf("====SEE YOU AGAIN====\n");
-            }
-            else{
-                printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+                if(IsStrEq(saveinput,firstword(input))){
+                    char* namaFile;
+                    namaFile = secondword(input);
+                    save(namaFile,arraygame);
+                    //save(namaFile,&arraygame);
+                }
+                else if(IsStrEq(creategameinput,input)){
+                    CreateGame(&arraygame);
+                }
+                else if(IsStrEq(listgameinput,input)){
+                    ListGame(&arraygame);
+                }
+                else if(IsStrEq(deletegameinput,input)){
+                    DeleteGame(&arraygame,&queue);
+                }
+                else if(IsStrEq(queuegameinput,input)){
+                    QueueGame(&arraygame,&queue);
+                }
+                else if(IsStrEq(playgameinput,input)){
+                    PlayGame(&queue);
+                }
+                else if(IsStrEq(helpinput,input)){
+                    help();
+                }
+                else if(IsStrEq(skipgameinput,firstword(input))){
+                    int manyskip;
+                    manyskip = atoi(secondword(input));
+                    SkipGame(&queue,manyskip);
+                }
+                else if(IsStrEq(quitinput,input)){
+                    printf("====SEE YOU AGAIN====\n");
+                }
+                else{
+                    printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+                }
+
             }
 
         }
-
-    }
-    else if(IsStrEq(loadinput,input)){
-        //load();
-        while(!IsStrEq(quitinput,input)){
-            printf("ENTER COMMAND : ");
-            input = scaninput();
-            if(IsStrEq(saveinput,firstword(input))){
-                char* namaFile;
-                namaFile = secondword(input);
-                save(namaFile,arraygame);
-            }
-            else if(IsStrEq(creategameinput,input)){
-                CreateGame(&arraygame);
-            }
-            else if(IsStrEq(listgameinput,input)){
-                ListGame(&arraygame);
-            }
-            else if(IsStrEq(deletegameinput,input)){
-                DeleteGame(&arraygame,&queue);
-            }
-            else if(IsStrEq(queuegameinput,input)){
-                QueueGame(&arraygame,&queue);
-            }
-            else if(IsStrEq(playgameinput,input)){
-                PlayGame(&queue);
-            }
-            else if(IsStrEq(helpinput,input)){
-                help();
-            }
-            else if(IsStrEq(skipgameinput,firstword(input))){
-                int manyskip;
-                manyskip = atoi(secondword(input));
-                SkipGame(&queue,manyskip);
-            }
-            else if(IsStrEq(quitinput,input)){
-                printf("====SEE YOU AGAIN====\n");
-            }
-            else{
-                printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+        else if(IsStrEq(loadinput,input)){
+            repeat = false;
+            //load();
+            while(!IsStrEq(quitinput,input)){
+                printf("ENTER COMMAND : ");
+                input = scaninput();
+                if(IsStrEq(saveinput,firstword(input))){
+                    char* namaFile;
+                    namaFile = secondword(input);
+                    save(namaFile,arraygame);
+                }
+                else if(IsStrEq(creategameinput,input)){
+                    CreateGame(&arraygame);
+                }
+                else if(IsStrEq(listgameinput,input)){
+                    ListGame(&arraygame);
+                }
+                else if(IsStrEq(deletegameinput,input)){
+                    DeleteGame(&arraygame,&queue);
+                }
+                else if(IsStrEq(queuegameinput,input)){
+                    QueueGame(&arraygame,&queue);
+                }
+                else if(IsStrEq(playgameinput,input)){
+                    PlayGame(&queue);
+                }
+                else if(IsStrEq(helpinput,input)){
+                    help();
+                }
+                else if(IsStrEq(skipgameinput,firstword(input))){
+                    int manyskip;
+                    manyskip = atoi(secondword(input));
+                    SkipGame(&queue,manyskip);
+                }
+                else if(IsStrEq(quitinput,input)){
+                    printf("====SEE YOU AGAIN====\n");
+                }
+                else{
+                    printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+                }
             }
         }
-    }
-    else if(IsStrEq(quitinput,input)){
-        printf("====SEE YOU AGAIN====\n");
-    }
-    else{
-        printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+        else if(IsStrEq(quitinput,input)){
+            repeat = false;
+            printf("====SEE YOU AGAIN====\n");
+        }
+        else{
+            printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+            printf("\n");
+            printf("====MAIN MENU====\n");
+            printf("1. START\n");
+            printf("2. LOAD\n");
+            printf("3. QUIT\n");
+            printf("ENTER COMMAND : ");
+            input = scaninput();
+        }
     }
 
     return 0;
