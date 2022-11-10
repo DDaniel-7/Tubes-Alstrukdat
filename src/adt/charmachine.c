5519c8dc516f0545733f81c5760626d71e0653e5
dwicakradaniel <110537201+DDaniel-7@users.noreply.github.com>
2022-11-10 20:27:10 +0700

    UPDATE

--- src/adt/charmachine.c
+++ src/adt/charmachine.c
@@ -5,7 +5,6 @@ char currentChar;
 boolean EOP;
 
 static FILE * pita;
-static FILE * filepita;
 static int retval;
 
 void START() {
@@ -30,36 +29,15 @@ void ADV() {
 
 	/* Algoritma */
 	retval = fscanf(pita,"%c",&currentChar);
+   EOP = (currentChar == NEWLINE);
 }
 
-void STARTFILE(char fileloc[]) {
-/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
-   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
-   I.S. : sembarang
-   F.S. : currentChar adalah karakter pertama pada pita
-          Jika currentChar != MARK maka EOP akan padam (false)
-          Jika currentChar = MARK maka EOP akan menyala (true) */
-
-	/* Algoritma */
-	filepita = fopen(fileloc, "r");
-	ADVFILE();
+char GetCC(){
+    return currentChar;
 }
+/* Mengirimkan currentChar */
 
-void ADVFILE(){
-/* Pita dimajukan satu karakter. 
-   I.S. : Karakter pada jendela = currentChar, currentChar != MARK
-   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama, 
-          currentChar mungkin = MARK
-		      Jika  currentChar = MARK maka EOP akan menyala (true) */
-
-	/* Algoritma */
-	retval = fscanf(filepita,"%c",&currentChar);
+boolean IsEOP(){
+   return EOP;
 }
-
-void CLOSE(){
-   fclose(pita);
-}
-
-void CLOSEFILE(){
-   fclose(filepita);
-}
+/* Mengirimkan true jika currentChar = MARK */
