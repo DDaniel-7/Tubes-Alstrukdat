d5a0008664c3f420b2b703272b2b01e557635e38
DzikriMuhammad15 <110533904+DzikriMuhammad15@users.noreply.github.com>
2022-11-11 18:35:45 +0700

    Add files via upload

---
+++ src/adt/driverwordmachine.c
@@ -0,0 +1,60 @@
+#include "boolean.h"
+#include "charmachine.h"
+#include "wordmachine.h"
+
+int main(){
+    boolean endWord;
+    Word currentWord;
+    static FILE *pita;
+    char *cek;
+    char *kataPertama;
+    char*kataKedua;
+    char *kataSatu;
+    char *kataDua;
+    char *kataTiga;
+    char *kataEmpat;
+    char *masukan;
+    char *angka;
+    int nomor;
+    int panjang;
+    char *cekPanjang;
+
+    STARTWORD();
+    // menuliskan kata yang berada dalam currentWord
+    printf("%s", currentWord.contents);
+    if(!endWord){
+        // masih ada kata berikutnya
+        // kata yang ditampilkan adalah kata selanjutnya
+        printf("%s", currentWord.contents);
+    }
+
+    cek = "kataPertama kataKedua";
+    kataPertama = firstword(cek);
+    kataKedua = secondWord(cek);
+    printf("%s", kataPertama);
+    printf("%s", kataKedua);
+
+    kataSatu = "sama";
+    kataDua = "sama";
+    kataTiga = "beda";
+    kataEmpat = "bedadong";
+
+    if(isStrEq(kataSatu, kataDua)){
+        // kata satu dan kata dua sama
+        printf("kata satu dan kata dua sama\n");
+    }
+    if(!isStrEq(kataDua, kataTiga)){
+        // kata dua dan kata tiga beda
+        printf("kata dua dan kata tiga beda\n");
+    }
+    // scan input dari dari pita menjadi sebuah masukan
+    masukan = scaninput();
+    angka = "1";
+    // mengkonversi sebuah string menjadi integer;
+    nomor = ScanNum(angka);
+    cekPanjang = "panjangKata";
+    panjang = LengthStr(cekPanjang);
+    printf("Panjang kata adalah : %d\n", panjang);
+
+    return 0;
+}
