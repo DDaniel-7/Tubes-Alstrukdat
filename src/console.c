5540acb3794d99dd930d038ef7452cb321c1729b
auvarifqi <88789984+auvarifqi@users.noreply.github.com>
2022-11-28 11:28:57 +0700

    Update console.c

--- src/console.c
+++ src/console.c
@@ -279,15 +279,19 @@ void help()
     printf("Berikut Pilihan Command yang Tersedia \n");
     printf("1. START : Membaca file konfigurasi sistem agar BNMO dapat berjalan\n");
     printf("2. LOAD : Membaca save file <filename> yang berisi list game yang dapat dimainkan, histori dan scoreboard game\n");
-    printf("3. SAVE : menyimpan state game pemain saat ini ke dalam suatu file\n");
-    printf("4. CREATEGAME : menambahkan game baru pada daftar game\n");
-    printf("5. LISTGAME : menampilkan daftar game yang disediakan oleh sistem\n");
-    printf("6. DELETEGAME : menghapus sebuah game dari daftar game\n");
-    printf("7. QUEUEGAME : mendaftarkan permainan kedalam list\n");
-    printf("8. PLAYGAME : memainkan sebuah permainan yang berada pada urutan paling atas\n");
-    printf("9. SKIPGAME : melewatkan permainan sebanyak n\n");
-    printf("10. QUIT : Keluar dari Program\n");
-    printf("11. HELP : Bantuan command-command pada BNMO\n");
+    printf("3. SAVE : Menyimpan state game pemain saat ini ke dalam suatu file\n");
+    printf("4. CREATEGAME : Menambahkan game baru pada daftar game\n");
+    printf("5. LISTGAME : Menampilkan daftar game yang disediakan oleh sistem\n");
+    printf("6. DELETEGAME : Menghapus sebuah game dari daftar game\n");
+    printf("7. QUEUEGAME : Mendaftarkan permainan kedalam list\n");
+    printf("8. PLAYGAME : Memainkan sebuah permainan yang berada pada urutan paling atas\n");
+    printf("9. SKIPGAME : Melewatkan permainan sebanyak n\n");
+    printf("10. SCOREBOARD : Melihat nama dan skor untuk semua game\n");
+    printf("11. RESET SCOREBOARD : melakukan reset terhadap scoreboard permainan\n");
+    printf("12. HISTORY : Melihat permainan apa saja yang telah dimainkan dari data yang sudah ada\n");
+    printf("13. RESET HISTORY : Menghapus semua history permainan yang dimainkan\n");
+    printf("14. QUIT : Keluar dari Program\n");
+    printf("15. HELP : Bantuan command-command pada BNMO\n");
 }
 
 
