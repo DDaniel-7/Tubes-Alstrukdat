28c086919020efc196c9c70cb487d38e023e8b6e
dwicakradaniel <110537201+DDaniel-7@users.noreply.github.com>
2022-11-29 19:48:19 +0700

    versi baru

--- src/game/toh.h
+++ src/game/toh.h
@@ -1,14 +1,16 @@
-# include "../adt/stack.h"
+# include "stack.h"
 # include "boolean.h"
 
 # ifndef TOH_H
 # define TOH_H
 
-void tampil(Stack s);
+void tampilkarakter(char c, int n);
 
-boolean isWin(Stack s, int disk);
+void tampil(Stack a, Stack b, Stack c, int n);
 
-void proses(Stack *s1, Stack *s2);
+void proses(Stack *s1, Stack *s2, int steps);
+
+int skor(int steps, int disk);
 
 void towerofhanoi();
 
