a2aa2897fc139071f5cde490ce3a173a537fbbe4
dwicakradaniel <110537201+DDaniel-7@users.noreply.github.com>
2022-12-01 20:36:19 +0700

    terbaru

--- src/adt/stack.c
+++ src/adt/stack.c
@@ -1,10 +1,15 @@
-/* implementasi stack.h */
+// NIM : 18221092
+// Nama : Dwicakra Danielle
+// Tanggal : 22 Oktober 2022
+// Topik Praktikum : pra praktikum
+// Deskripsi : implementasi stack.h
+
 #include <stdio.h>
 #include "boolean.h"
 #include "stack.h"
 
 /* *** Konstruktor/Kreator *** */
-void CreateEmptytoh (Stack *S)
+void CreateEmptytoh (Stackt *S)
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0.. MaxEl */
@@ -15,20 +20,20 @@ void CreateEmptytoh (Stack *S)
 }
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsEmptyStacktoh (Stack S)
+boolean IsEmptyStacktoh (Stackt S)
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
 {
     return (Top(S)==Nil);
 }
 
-boolean IsFulltoh (Stack S)
+boolean IsFulltoh (Stackt S)
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 {
     return (Top(S)==MaxEl-1);
 }
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Pushtoh (Stack * S, infotype X)
+void Pushtoh (Stackt * S, infotypes X)
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
@@ -39,7 +44,7 @@ void Pushtoh (Stack * S, infotype X)
 }
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Poptoh (Stack * S, infotype* X)
+void Poptoh (Stackt * S, infotypes* X)
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
