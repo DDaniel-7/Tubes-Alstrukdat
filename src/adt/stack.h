4738a96145de944b4624efd36f227e265b7a975e
dwicakradaniel <110537201+DDaniel-7@users.noreply.github.com>
2022-12-01 20:35:51 +0700

    terbaru

--- src/adt/stack.h
+++ src/adt/stack.h
@@ -11,16 +11,16 @@
 #define MaxEl 100
 /* Nil adalah stack dengan elemen kosong . */
 
-typedef int infotype;
+typedef int infotypes;
 typedef int address;   /* indeks tabel */
 
 /* Contoh deklarasi variabel bertype stack dengan ciri TOP : */
 /* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
 typedef struct { 
-  infotype T[MaxEl]; /* tabel penyimpan elemen */
+  infotypes T[MaxEl]; /* tabel penyimpan elemen */
   address TOP;  /* alamat TOP: elemen puncak */
   int ctr;
-} Stack;
+} Stackt;
 /* Definisi stack S kosong : S.TOP = Nil */
 /* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
 /* Jika S adalah Stack maka akses elemen : */
@@ -34,26 +34,26 @@ typedef struct {
 
 /* ************ Prototype ************ */
 /* *** Konstruktor/Kreator *** */
-void CreateEmptytoh(Stack *S);
+void CreateEmptytoh(Stackt *S);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0.. MaxEl */
 /* Ciri stack kosong : TOP bernilai Nil */
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsEmptytoh(Stack S);
+boolean IsEmptyStacktoh(Stackt S);
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
-boolean IsFulltoh(Stack S);
+boolean IsFulltoh(Stackt S);
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Pushtoh(Stack * S, infotype X);
+void Pushtoh(Stackt * S, infotypes X);
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Poptoh(Stack * S, infotype* X);
+void Poptoh(Stackt * S, infotypes* X);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
