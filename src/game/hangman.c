2417ff1c87727b48d7828de3fea1b533260eeb25
dwicakradaniel <110537201+DDaniel-7@users.noreply.github.com>
2022-12-02 18:26:53 +0700

    yang udh bener

--- src/game/hangman.c
+++ src/game/hangman.c
@@ -1,170 +1,287 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "boolean.h"
-#include <math.h>
 #include <time.h>
-#include "charmachine.h"
 #include "wordmachine.h"
+#include "charmachine.h"
+# include "hangman.h"
 
-#define WORDS 10
-#define WORDLEN 40
-#define CHANCE 10
-
-long long mystrlen(char *str) 
-{
-    int count = 0;
-    while(*str !='\0')
-    {
-        count++;
-        str++;
-    }
-    return count;
-}
-
-int irand(int i) {
-	boolean srandc = false;
-    if (!srandc) {
-        srand(time(NULL));
-        srandc = true;
-    }
-    return rand() % i;
-}
-
-char* decrypt(char* code) {
-	int hash = ((mystrlen(code) - 3) / 3) + 2;
-	char* decrypt = malloc(hash);
-	char* toFree = decrypt;
-	char* word = code;
-	for (int ch = *code; ch != '\0'; ch = *(++code))
+void gameFunction(char *wordinFunction, char *userWordinFunction, int letterNumber) {     
+	int totalTrue= 0;			
+	int totalFail= 0;			
+	char scannedChar;
+	
+	while(totalFail< 10)
 	{
-		if((code - word + 2) % 3  == 1){
-			*(decrypt++) = ch - (word - code + 1) - hash;
-		}
-	}
-	*decrypt = '\0';
-	return toFree;
-}
-
-void tampilhangman(int wrong, char* body) {
-	printf("\tMistakes :%d\n", wrong);
-	switch(wrong) {
-        case 10: body[10] = '\\'; break;
-        case 9: body[9] = '/'; break;
-        case 8: body[8] ='\\'; break;
-        case 7: body[7] = '|'; break;
-		case 6: body[6] = '/'; break;
-		case 5: body[5] = ')', body[4] = '('; break;
-		case 4: body[3] = '|'; break;
-		case 3: body[2] = '_'; break;
-		case 2: body[1] = '_'; break;
-		case 1: body[0] = '_'; break;
-		default: break;
-
-	}
-	printf("\t _______%c%c%c\n"
-	       "\t|         %c\n"
-	       "\t|        %c %c\n"
-	       "\t|        %c%c%c\n"
-	       "\t|        %c %c\n"
-	       "\t|             \n"
-	       "\t|             ", body[0], body[1], body[2],
-	       body[3], body[4], body[5], body[6],body[7],body[8],body[9],body[10]);
-}
-
-void tampilkata(char* guess, int len) {
-	printf("\tKATA : ");
-	for (int i = 0; i < len; ++i)
-	{
-		printf(" %c ", guess[i]);
-	}
-	printf("\n");
-}
-
-void *mymemset(void *b, int c, int len)
-{
-  int i;
-  char *p = b;
-  i = 0;
-  while(len > 0)
-    {
-      *p = c;
-      p++;
-      len--;
-    }
-  return(b);
-}
-
-char *mystrchr(char *s,char c)
-{
-   while(*s != c && *s != '\0') {
-      s++;
-   }
-   if(*s == c) {
-      return s;
-   }else {
-      return NULL;
-   }
-}
-
-void hangman() {
-	printf("\tSELAMAT DATANG DI HANGMAN\n\n");
-	char values[WORDS][WORDLEN] = {"N~mqOlJ^tZletXodeYgs","gCnDIfFQe^CdP^^B{hZpeLA^hv","7urtrtwQv{dt`>^}FaR]i]XUug^GI"
-								    ,"cruD=idduvUdr=gmcauCmg]","BQt`zncypFVjvIaTl]u=_?Aa}F",
-									"iLvkKdT`yu~mWj[^gcO|","jSiLyzJ=vPmnv^`N]^>ViAC^z_","xo|RqqhO|nNstjmzfiuoiFfhwtdh~",
-									"OHkttvxdp|[nnW]Drgaomdq"};
-	char *body = malloc(CHANCE+1);
-	int id = irand(WORDS);
-	char *word = decrypt(values[id]);
-	int len = mystrlen(word);
-	char *guessed = malloc(len);
-	char falseWord[CHANCE];
-
-	mymemset(body, ' ', CHANCE+1);
-	mymemset(guessed, '_', len);
-	char guess;
-	boolean found;
-	char* win;
-
-	int mistakes = 0;
-	setvbuf(stdin, NULL, _IONBF, 0);
-	while(mistakes < CHANCE && win != NULL) {
-		found = false;
-		printf("\n");
-		tampilhangman(mistakes, body);
-		printf("\n");
-		printf("\tKARAKTER YANG SALAH : ");
-		for (int i = 0; i < mistakes; ++i){
-			printf("%c", falseWord[i]);
-		}
-		printf("\n");
-		tampilkata(guessed, len);
-		printf("\tMASUKAN TEBAKAN : ");
-		do {guess = *scaninput();} while ( getchar() != '\n' );
-		for (int i = 0; i < len; ++i)
+		printFigure(totalFail);		
+		printf("\n\n\n");
+		printUserWord(userWordinFunction, letterNumber);    
+		printf("\nMASUKAN TEBAKAN: ");
+		scannedChar = *scaninput();
+		scannedChar= toupper(scannedChar);
+	
+		int letterAlreadyUsed= 1;			
+		while(letterAlreadyUsed== 1)
 		{
-			if(word[i] == guess) {
-				found = true;
-				guessed[i] = guess;
+			for(int i=  0; i< letterNumber; i++)
+			{
+				if(userWordinFunction[i]!= scannedChar)
+				{
+					letterAlreadyUsed= 0;	
+				}
+				else
+				{
+					letterAlreadyUsed= 1;
+					printFigure(totalFail);
+					printf("\n\n\n");
+					printUserWord(userWordinFunction, letterNumber);
+					printf("\nSUDAH ADA!\nMASUKAN TEBAKAN LAIN!");
+					scannedChar = *scaninput();
+					scannedChar= toupper(scannedChar);
+					break;
+				}
+			}
+		
+		}
+	
+		int letterTrueCheck= 0;                       
+		for(int i= 0; i< letterNumber; i++)			   
+		{
+			if(scannedChar== wordinFunction[i])		 
+			{
+				userWordinFunction[i]= wordinFunction[i];
+				letterTrueCheck++;
+				totalTrue++;				
 			}
 		}
-		if(!found) {
-			falseWord[mistakes] = guess;
-			mistakes += 1;
+		
+		if(letterTrueCheck== 0)					
+		{
+			totalFail++;
 		}
-		win = mystrchr(guessed, '_');
+		
+		if(totalTrue == letterNumber)             
+		{
+			printFigure(totalFail);
+			printf("\n\n\n");
+			printUserWord(userWordinFunction, letterNumber);
+			printf("\n\nBerhasil menebak kata %s! Kamu mendapatkan %d poin!",wordinFunction, totalTrue);
+			return;
+		}
 	}
-
-	if(win == NULL){
-        printf("\n");
-        printf("\tBerhasil menebak kata %s! Kamu mendapatkan %d poin!", word, len);
-    }
-    else {
-        printf("\n");
-        tampilhangman(mistakes, body);
-        printf("\n\tKamu kurang beruntung. katanya adalah %s\n", word);
-    }
-    free(body);
-	free(word);
-	free(guessed);
+	
+	printFigure(totalFail);	                        
+	printf("\n\n\n");
+	for(int i= 0; i< letterNumber; i++)
+	{
+		printf("%c ", wordinFunction[i]);
+	}
+	printf("\n\nKATANYA ADALAH %s ", wordinFunction);	
+    printf("\n SKOREMU : %d", totalTrue);	
+	return;
+	
 }
 
+void printUserWord(char *userWordinPrint,int letterNumber)   
+{
+	for(int i= 0; i< letterNumber; i++)
+	{
+		printf("%c ", userWordinPrint[i]);
+	}
+}
+
+void printFigure(int failinFigure)    
+{	
+	switch(failinFigure)
+	{
+        case 0:
+            printf("\n\n\n\n\n        \n        \n        \n     __ __");
+			break;
+		case 1:
+			printf("\n\n\n\n\n        \n        \n       |\n     __|__");
+			break;
+		case 2:
+			printf("\n\n\n\n\n       |\n       |\n       |\n     __|__");
+			break;
+		case 3:
+			printf("  _ _ _\n       |\n       |\n       |\n       |\n       |\n       |\n       |\n     __|__ ");
+			break;
+		case 4: 
+			printf("  _ _ _  \n |     | \n |     | \n       |\n       |\n       |\n       |\n       |\n     __|__ ");
+			break;
+		case 5:
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n       |\n       |\n       |\n       |\n     __|__ ");
+			break;
+		case 6:
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/      |\n       |\n       |\n       |\n     __|__ ");
+			break;
+		case 7:
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/ \\    |\n       |\n       |\n       |\n     __|__ ");
+			break;
+		case 8:
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/|\\    |\n |     |\n |     |\n       |\n     __|__ ");
+			break;
+		case 9:
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/|\\    |\n |     |\n |     |\n/      |\n     __|__ ");
+			break;
+		case 10:
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/|\\    |\n |     |\n |     |\n/ \\    |\n     __|__ ");
+			break;
+	}
+}
+
+
+void hangman()
+{
+	FILE *fpointer;     
+	fpointer = fopen("hangmanwords.txt", "r");
+	if(fpointer== NULL)
+	{
+		printf("hangmanwords.txt not found");
+		return 0;
+	}
+	char readFromFile[300];
+	while(!feof(fpointer))
+	{
+		fgets(readFromFile, 300, fpointer);
+	}
+	fclose(fpointer);
+	srand(time(NULL));  
+    printf("Main Menu:\n");
+    printf("1. Play Game\n");
+    printf("2. Exit\n");
+    printf("PILIH (1/2): ");
+    STARTWORD();
+    while (IsWordStr(currentWord, "2")){
+        break;
+    }
+    if (IsWordStr(currentWord, "1")) {
+        int restartGame = 1; 
+        while(restartGame==1)
+        {
+            int gameType;   
+            printf(" WELCOME TO HANGMAN!\n\nChoose a game\n1. Six Letters Game\n2. Seven Letters Game\n3. Random Game\n");
+            printf("ENTER CHOICE: ");
+            ADVWORD();
+            gameType = ScanNum(currentWord);
+            while(gameType != 1 && gameType != 2 && gameType != 3)
+            {
+                printf("WELCOME TO HANGMAN!\n\nChoose a game\n1. Six Letters Game\n2. Seven Letters Game\n3. Random Game\n");
+                printf("ENTER CHOICE: ");
+                ADVWORD();
+                gameType = ScanNum(currentWord);
+            }
+            if(gameType == 3)
+            {
+                gameType = rand() % 2 + 1;
+            }
+        
+            int categoryType;
+            printf("\n\nChoose a category\n1. Fruits and Vegetables\n2. Sports\n3. Technological Devices\n4. Countries\n5. Random Category\n");
+            printf("PICK CATEGORY: ");
+            ADVWORD();
+            categoryType = ScanNum(currentWord);
+        
+            while(categoryType != 1 && categoryType != 2 && categoryType != 3 && categoryType != 4 && categoryType != 5)
+            {
+
+                printf("\n\nChoose a category\n1. Fruits and Vegetables\n2. Sports\n3. Technological Devices\n4. Countries\n5. Random Category\n");	
+                printf("PICK CATEGORY: ");	
+                ADVWORD();
+                categoryType = ScanNum(currentWord);
+            }
+            if(categoryType== 5)
+            {
+                categoryType= rand() % 4 + 1;
+            }
+        
+            char askedWord[8];								
+            char userWord[8]= {'_','_','_','_','_','_','_'};   
+            
+            int randomer = rand()% 5;                          
+            
+            if(categoryType == 1)      
+            {	
+                if(gameType== 1)
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 0)+ (randomer * 7)+ i];
+                    }	
+                }
+                else
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 1)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+            }
+            else if(categoryType == 2) 
+            {		
+                if(gameType==1)
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 2)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+                
+                else
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 3)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+            }
+            else if(categoryType == 3) 
+            {
+                if(gameType==1)
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 4)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+                
+                else
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 5)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+            }
+            else			        
+            {
+                if(gameType==1)
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 6)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+                
+                else
+                {
+                    for(int i= 0; i< 7; i++)
+                    {
+                        askedWord[i]= readFromFile[(35* 7)+ (randomer * 7)+ i];
+                    }
+                    
+                }
+            }
+            
+            gameFunction(askedWord, userWord, (gameType+ 5));    
+            printf("\n\nDO YOU WANT TO CONTINUE? (1.YES/2.NO) : ");    
+            ADVWORD();
+            restartGame = ScanNum(currentWord);
+        }
+    }
+}
