d787be09d51dc151071fe213424e7999f9df4ff4
johanchristiansen <18221088@std.stei.itb.ac.id>
2022-12-02 19:20:20 +0700

    up

--- src/game/hangman.h
+++ src/game/hangman.h
@@ -1,7 +1,8 @@
 #ifndef hangman_H
 #define hangman_H
 #include "boolean.h"
-
+#include "../adt/arrayMap.h"
+#include "../console.h"
 
 void gameFunction(char *wordinFunction, char *userWordinFunction, int letterNumber);   
 
@@ -9,6 +10,6 @@ void printUserWord(char * userWordinPrint,int letterNumber);
 
 void printFigure(int failinFigure);		
 
-void hangman();
+void hangman(TabMap *arrmapsb);
 
 #endif
